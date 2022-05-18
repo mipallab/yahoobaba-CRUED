a012c6d2a5c051adb6f1986ff4b80900c655bebb
pallab321 <pallab@qmail.com.bd>
2022-05-18 22:15:47 +0600

    Home page / data read php done

--- index.php
+++ index.php
@@ -4,71 +4,53 @@ include 'header.php';
 <div id="main-content">
     <h2>All Records</h2>
     <table cellpadding="7px">
+
+        <?php 
+            $connect = mysqli_connect("localhost", "root", "", "crud") or die("Database Connection Faield");
+            $query = "SELECT * FROM students JOIN studentclass WHERE students.sclass = studentclass.c_code";
+            $result = mysqli_query($connect, $query) or die("Query Field!!!");
+
+            if(mysqli_num_rows($result) > 0) {
+        ?>
+
         <thead>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Class</th>
-        <th>Phone</th>
-        <th>Action</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Class</th>
+            <th>Phone</th>
+            <th>Action</th>
         </thead>
         <tbody>
+
+            <?php
+                while($row = mysqli_fetch_assoc($result)) {
+            ?>
+
             <tr>
-                <td>1</td>
-                <td>Ramesh</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
+                <td><?php echo $row['sid'];?></td>
+                <td><?php echo $row['sname'];?></td>
+                <td><?php echo $row['saddress'];?></td>
+                <td><?php echo $row['c_name'];?></td>
+                <td><?php echo $row['sphone'];?></td>
                 <td>
                     <a href='edit.php'>Edit</a>
                     <a href='delete-inline.php'>Delete</a>
                 </td>
             </tr>
-            <tr>
-                <td>2</td>
-                <td>Suresh</td>
-                <td>Punjab</td>
-                <td>BCOM</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Suresh</td>
-                <td>Haryana</td>
-                <td>BSC</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Krishna</td>
-                <td>Gujrat</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Rohit</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
+            <?php
+                }
+            ?>
+            
         </tbody>
+
+        <?php 
+            }else {
+                echo "<h2>No Data Found !!! </h2>";
+            }
+            mysqli_close($connect);
+        ?>
+
     </table>
 </div>
 </div>
